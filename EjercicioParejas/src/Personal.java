0533f55a0243afd16d914c57ee8a6f912df8dd2a
pepirlo <pepe@MacBook-Pro-de-Pepe.local>
2018-02-26 12:56:44 +0100

    ssss

--- EjercicioParejas/src/Personal.java
+++ EjercicioParejas/src/Personal.java
@@ -1,5 +1,58 @@
 
 public class Personal {
-	String nombre;
-	String apellidos;
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private int id;
+	
+	/**
+	 * @param nombre
+	 * @param apellidos
+	 * @param dni
+	 * @param id
+	 */
+	public Personal(String nombre, String apellidos, String dni, int id) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
 }
